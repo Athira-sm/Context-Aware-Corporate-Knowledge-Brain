e4def3c2d4252810f1aaf9589aaf541eb8fe6fdc
Athira S M <athirasm31@gmail.com>
2026-02-11 21:16:03 +0530

    first commit

---
+++ backend/routes/sopRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const multer = require("multer");
+const { uploadSOP } = require("../controllers/sopController");
+
+const router = express.Router();
+
+const upload = multer({
+  dest: "uploads/",
+});
+
+router.post("/upload", upload.single("file"), uploadSOP);
+
+module.exports = router;
